1545327ed6ddfff11a4e4665f222a5860e5c940b
Joseph Cameron <jf.cameron@gmail.com>
2017-12-14 16:33:36 -0800

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+module.exports = 
+{
+    entry: './src/index.js',
+
+    output: 
+    {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
